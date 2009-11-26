7700d35818dacadda11549fe49bef8352f99597d
Dan Garner <dan@xibo.org.uk>
2009-11-26 22:01:46 +0000

    [dotnetclient] Improved CacheManager to recalc MD5's if the file in question has changed.

--- dotNET/CacheManager.cs
+++ dotNET/CacheManager.cs
@@ -46,10 +46,29 @@ namespace XiboClient
             {
                 if (file.path == path)
                 {
+                    // Check to see if this file has been modified since the MD5 cache
+                    DateTime lastWrite = File.GetLastWriteTime(Properties.Settings.Default.LibraryPath + @"\" + path);
+
+                    if (lastWrite > file.cacheDate)
+                    {
+                        // Get the MD5 again
+                        return CalcMD5(path);
+                    }
+
                     return file.md5;
                 }
             }
 
+            return CalcMD5(path);
+        }
+
+        /// <summary>
+        /// Calculates the MD5 for a path
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        private string CalcMD5(String path)
+        {
             // We need to generate the MD5 and store it for later
             Md5Resource md5Resource = new Md5Resource();
 
@@ -58,6 +77,9 @@ namespace XiboClient
             using (FileStream md5Fs = new FileStream(Properties.Settings.Default.LibraryPath + @"\" + path, FileMode.Open, FileAccess.Read))
             {
                 md5Resource.md5 = Hashes.MD5(md5Fs);
+                md5Resource.cacheDate = DateTime.Now;
+
+                // Add the resource to the collection
                 _files.Add(md5Resource);
 
                 return md5Resource.md5;
@@ -69,5 +91,6 @@ namespace XiboClient
     {
         public String md5;
         public String path;
+        public DateTime cacheDate;
     }
 }
