a72e93f64da73842b649150df5e9205a5018b811
Dan Garner <dan@xibo.org.uk>
2011-06-05 18:32:08 +0100

    [dotnetclient] Added missing files

---
+++ dotNET/MediaOption.cs
@@ -0,0 +1,93 @@
+/*
+ * Xibo - Digitial Signage - http://www.xibo.org.uk
+ * Copyright (C) 2006 - 2011 Daniel Garner
+ *
+ * This file is part of Xibo.
+ *
+ * Xibo is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version. 
+ *
+ * Xibo is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Xibo.  If not, see <http://www.gnu.org/licenses/>.
+ */
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Security.Cryptography;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Serialization;
+using System.Diagnostics;
+using System.Collections;
+
+namespace XiboClient
+{
+    class MediaDictionary
+    {
+        private List<MediaOption> _options;
+
+        public MediaDictionary()
+        {
+            _options = new List<MediaOption>();
+        }
+
+        public void Add(string name, string value)
+        {
+            MediaOption option = new MediaOption();
+            option.Name = name;
+            option.Value = value;
+
+            _options.Add(option);
+        }
+
+        public void Clear()
+        {
+            _options.Clear();
+        }
+
+        public int Count
+        {
+            get
+            {
+                return _options.Count;
+            }
+        }
+
+        public string Get(string name)
+        {
+            foreach (MediaOption option in _options)
+            {
+                if (option.Name == name)
+                    return option.Value;
+            }
+
+            throw new IndexOutOfRangeException("No such option");
+        }
+
+        public string Get(string name, string def)
+        {
+            foreach (MediaOption option in _options)
+            {
+                if (option.Name == name)
+                    return option.Value;
+            }
+
+            return def;
+        }
+    }
+
+    struct MediaOption
+    {
+        public string Name;
+        public string Value;
+    }
+}
