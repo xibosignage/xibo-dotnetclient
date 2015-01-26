b0110455458a7260c1eae6e9c6ca8db44ddd1a48
Dan Garner <dan@springsignage.com>
2015-01-26 10:51:52 +0000

    Bumped Version.

--- Logic/ApplicationSettings.cs
+++ Logic/ApplicationSettings.cs
@@ -36,9 +36,9 @@ namespace XiboClient
         private static string _default = "default";
 
         // Application Specific Settings we want to protect
-        private string _clientVersion = "1.7.0-rc1";
+        private string _clientVersion = "1.7.0";
         private string _version = "4";
-        private int _clientCodeVersion = 103;
+        private int _clientCodeVersion = 104;
 
         public string ClientVersion { get { return _clientVersion; } }
         public string Version { get { return _version; } }
