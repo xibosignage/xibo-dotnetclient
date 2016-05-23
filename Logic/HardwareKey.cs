9759f4b27f3709f7e2b459853be5d356faa9f160
Dan Garner <dan@springsignage.com>
2016-05-23 11:04:56 +0100

    Fix for mac address formatting xibosignage/xibo#785

--- Logic/HardwareKey.cs
+++ Logic/HardwareKey.cs
@@ -132,7 +132,7 @@ namespace XiboClient
             {
                 if (nic.OperationalStatus == OperationalStatus.Up)
                 {
-                    macAddresses += nic.GetPhysicalAddress().ToString();
+                    macAddresses += BitConverter.ToString(nic.GetPhysicalAddress().GetAddressBytes()).Replace('-', ':');
                     break;
                 }
             }
