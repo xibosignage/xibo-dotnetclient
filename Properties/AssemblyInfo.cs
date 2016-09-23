0b8da5267d9cb4606dc81073c3b43b1fb295bc78
Dan Garner <dan@springsignage.com>
2016-09-23 14:25:55 +0100

    Set assembly version correctly. xibosignage/xibo#834

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -30,6 +30,6 @@ using System.Resources;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyVersion("1.7.9.0")]
+[assembly: AssemblyFileVersion("1.7.9.0")]
 [assembly: NeutralResourcesLanguageAttribute("en-GB")]
