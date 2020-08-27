9941b80123dc5e3012b6a29f14f041df2107b33e
Dan Garner <dan@xibosignage.com>
2020-08-27 15:46:05 +0100

    Implement /info route and protect system files in the library #160

---
+++ Control/InfoController.cs
@@ -0,0 +1,53 @@
+﻿/**
+ * Copyright (C) 2020 Xibo Signage Ltd
+ *
+ * Xibo - Digital Signage - http://www.xibo.org.uk
+ *
+ * This file is part of Xibo.
+ *
+ * Xibo is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * Xibo is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Xibo.  If not, see <http://www.gnu.org/licenses/>.
+ */
+using EmbedIO;
+using EmbedIO.Routing;
+using EmbedIO.WebApi;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.IO;
+using System.Text;
+using XiboClient.Log;
+
+namespace XiboClient.Control
+{
+    class InfoController : WebApiController
+    {
+        [Route(HttpVerbs.Get, "/")]
+        public void GetInfo()
+        {
+            Response.ContentType = MimeType.Json;
+            using (var writer = HttpContext.OpenResponseText(Encoding.UTF8, true))
+            {
+                writer.Write(
+                    JObject.FromObject(new
+                    {
+                        hardwareKey = ApplicationSettings.Default.HardwareKey.ToString(),
+                        displayName = ApplicationSettings.Default.DisplayName,
+                        timeZone = ApplicationSettings.Default.DisplayTimeZone,
+                        latitude = ClientInfo.Instance.CurrentGeoLocation.Latitude,
+                        longitude = ClientInfo.Instance.CurrentGeoLocation.Longitude
+                    })
+                .ToString());
+            }
+        }
+    }
+}
---
+++ Control/RestrictiveFileModule.cs
@@ -0,0 +1,33 @@
+﻿using EmbedIO;
+using EmbedIO.Files;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XiboClient.Control
+{
+    class RestrictiveFileModule : FileModule
+    {
+        private List<string> _restrictedPaths;
+
+        public RestrictiveFileModule(string baseRoute, IFileProvider provider, List<string> restrictedPaths)
+            : base(baseRoute, provider)
+        {
+            _restrictedPaths = restrictedPaths;
+        }
+
+        protected override async Task OnRequestAsync(IHttpContext context)
+        {
+            if (_restrictedPaths.Any(o => context.RequestedPath.StartsWith(o)))
+            {
+                throw HttpException.Forbidden();
+            }
+            else
+            {
+                await base.OnRequestAsync(context);
+            }
+        }
+    }
+}
