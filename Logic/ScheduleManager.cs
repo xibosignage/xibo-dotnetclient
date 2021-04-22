cdd1456813880baf1148e387e340d5278aabe0d7
Dan Garner <dan@xibosignage.com>
2021-04-22 13:02:45 +0100

    Fix for underplaying interrupts #213

--- Logic/ScheduleManager.cs
+++ Logic/ScheduleManager.cs
@@ -760,8 +760,11 @@ namespace XiboClient
 
             // Now we combine both schedules together, spreading the interrupts evenly
             int pickCount = Math.Max(resolvedNormal.Count, resolvedInterrupt.Count);
-            int normalPick = (int)Math.Floor(1.0 * pickCount / resolvedNormal.Count);
-            int interruptPick = (int)Math.Floor(1.0 * pickCount / resolvedInterrupt.Count);
+
+            // We use ceiling here so that in the case of uneven lists we pick more aggressively
+            // using floor would mean a schedule longer than the hour overall and interrupts may underplay
+            int normalPick = (int)Math.Ceiling(1.0 * pickCount / resolvedNormal.Count);
+            int interruptPick = (int)Math.Ceiling(1.0 * pickCount / resolvedInterrupt.Count);
             int normalIndex = 0;
             int interruptIndex = 0;
 
