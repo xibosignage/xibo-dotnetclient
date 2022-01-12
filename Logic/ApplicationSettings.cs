aa769753bcdcde9b83570ccadae61de33de12e37
Dan Garner <dan@xibosignage.com>
2022-01-12 15:00:56 +0000

    Bump to 302.4. Update libraries.

--- Logic/ApplicationSettings.cs
+++ Logic/ApplicationSettings.cs
@@ -52,7 +52,7 @@ namespace XiboClient
         /// </summary>
         private List<string> ExcludedProperties;
 
-        public string ClientVersion { get; } = "3 R302.3";
+        public string ClientVersion { get; } = "3 R302.4";
         public string Version { get; } = "6";
         public int ClientCodeVersion { get; } = 302;
 
